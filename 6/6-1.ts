54b52ccd373675141d21f754c27318975bbd7b87
Saerom Park <park.srome@gmail.com>
2020-12-06 01:51:45 -0600

    set later

--- 6/6-1.ts
+++ 6/6-1.ts
@@ -2,16 +2,16 @@ import { readFileSync } from 'fs';
 
 const inputFile = process.argv.slice(2)[0];
 
-const data: Set<string>[][] = readFileSync(inputFile, 'utf8')
+const data: string[][][] = readFileSync(inputFile, 'utf8')
   .split('\n\n')
   .map(str => str.split('\n'))
-  .map(arr => arr.map(str => new Set(str.split(''))));
+  .map(arr => arr.map(str => str.split('')));
 
 console.log(solve(data));
 
-function solve(data: Set<string>[][]): number {
-  return data.map((group: Set<string>[]): Set<string> =>
-      group.reduce((acc, curr) => new Set([...acc, ...curr]))
+function solve(data: string[][][]) {
+  return data.map((group: string[][]): Set<string> =>
+      new Set(group.reduce((acc, curr) => [...acc, ...curr]))
     )
     .map(set => set.size)
     .reduce((acc, curr) => acc + curr);
